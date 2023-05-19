90705b1b37c3de4f8e6bb8df31d63043bf47b707
Julian Haag <me@jhaag.me>
2023-05-19 03:09:35 +0200

    Add Permissions

--- src/test/kotlin/net/spaceblock/utils/di/commands/CommandTestController.kt
+++ src/test/kotlin/net/spaceblock/utils/di/commands/CommandTestController.kt
@@ -12,41 +12,41 @@ class CommandTestController(
     var i = 0
 
     @Command("test", playerOnly = true)
-    fun test(player: CommandSender) {
+    fun test() {
         println("test")
         i++
     }
 
     @TabComplete("test")
-    fun testTab(player: CommandSender): List<String> {
+    fun testTab(): List<String> {
        return listOf("test")
     }
 
     @Command("test2", playerOnly = true)
-    suspend fun test2(player: Player) {
+    suspend fun test2() {
         println("test2")
         i++
     }
 
     @Command("test-op", playerOnly = true)
     @IsOp
-    suspend fun op(player: Player) {
+    suspend fun op() {
         println("test-op")
     }
 
     @Command("test-permission", playerOnly = true)
     @HasPermission("test")
-    suspend fun permission(player: Player) {
+    suspend fun permission() {
         println("test-permission")
     }
 
     @Command("test3", playerOnly = true)
-    fun test3(player: CommandSender): Boolean {
+    fun test3(): Boolean {
         return false
     }
 
     @Command("test4", playerOnly = true)
-    suspend fun test4(player: CommandSender): Boolean {
+    suspend fun test4(): Boolean {
         delay(1)
         return false
     }
